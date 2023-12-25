79f7498dece709b5f8b537711c5a2c7a184e55c1
hungngooo <ngongochung531@gmail.com>
2023-12-25 15:07:47 +0700

    Lan thu 2 lam chuyen ay - quen roi

--- src/com/fu/mathutil/MathUtilLive.java
+++ src/com/fu/mathutil/MathUtilLive.java
@@ -18,6 +18,7 @@ public class MathUtilLive {
     public static void main(String[] args) {
         long result = MathUtil.getFactorial(5);
         System.out.println("5! = 120; actual: " + result);
+        // 3:07:00
     }
     
 }
